0d6a08c0b04e3ebd70ec3741682c408e9e369142
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-05-10 07:18:37 +0000

    feat: add env schema and server.ts

---
+++ api/src/schemas/env.schema.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+import 'dotenv/config'
+
+const envVariables = z.object({
+    NODE_ENV: z.string(),
+    PORT: z.number(),
+    DATABASE_URL: z.string(),
+    JWT_SECRET_KEY: z.string(),
+    CLOUDINARY_CLOUD_NAME: z.string(),
+    CLOUDINARY_API_KEY: z.string(),
+    CLOUDINARY_API_SECRET: z.string(),
+})
+
+export const env = envVariables.parse(process.env)
