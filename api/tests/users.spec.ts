0fb10dea0a0700cf4a6b444e020942a7976b382d
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-05-17 19:30:43 +0530

    feat: add test for update profile and FR list feature

--- api/tests/users.spec.ts
+++ api/tests/users.spec.ts
@@ -1,12 +1,24 @@
 import app from '../src/app'
-import { expect, describe, it, beforeAll, afterAll, afterEach, beforeEach } from 'vitest'
+import { expect, describe, it, beforeAll, afterAll, afterEach, beforeEach, vi } from 'vitest'
 import request from "supertest"
 import { prisma } from "../src/libs/prisma"
 import { createMockUser, createMockPost } from "../src/scripts/mock-data"
 import { env } from '../src/schemas/env.schema'
 import jwt from "jsonwebtoken"
+import type { ValidationError } from "../src/types/index"
+import type { Follow } from '@prisma/client'
 
-const mockUsers = Array.from({ length: 10 }).map(() => createMockUser());
+// mock uploadOnCloudinary function
+
+vi.mock("../src/utils/cloudinary", () => {
+    return {
+        uploadOnCloudinary: vi.fn().mockResolvedValue({
+            secure_url: "https://cloudinary.com"
+        })
+    }
+})
+
+const mockUsers = Array.from({ length: 5 }).map(() => createMockUser());
 
 beforeAll(async () => await prisma.user.createMany({ data: mockUsers }))
 afterAll(async () => await prisma.user.deleteMany({}))
@@ -116,3 +128,96 @@ describe("DELETE /users/:userId/follow-request", () => {
     })
 })
 
+describe("PATCH /users/me", () => {
+    const [user] = mockUsers;
+    const token = jwt.sign({ id: user.id }, env.JWT_SECRET_KEY);
+
+    it("should respond with 422 Unprocessable Entity and a validation error when the 'fullname' field is empty", async () => {
+        const res = await request(app)
+            .patch(`/users/me`)
+            .set('Authorization', `Bearer ${token}`)
+            .field("fullname", "")
+            .field("about", "Lorem ipsum")
+
+        const fullnameError = res.body.errors.find((e: ValidationError) => e.fieldName === "fullname")
+
+        expect(res.status).toBe(422)
+        expect(fullnameError.message).toBe("Name field can't be empty")
+    })
+
+    it("should respond with 200 OK and return the updated user profile when valid text data is provided", async () => {
+        const res = await request(app)
+            .patch(`/users/me`)
+            .set('Authorization', `Bearer ${token}`)
+            .field("fullname", "John doe")
+            .field("about", "Lorem ipsum")
+
+        expect(res.status).toBe(200)
+        expect(res.body.user.fullname).toBe("John doe")
+        expect(res.body.user.about).toBe("Lorem ipsum")
+    })
+
+    it("should respond with 415 Unsupported Media Type when the uploaded avatar file is not an image", async () => {
+        const mockBuffer = Buffer.from('mock-data');
+
+        const res = await request(app)
+            .patch(`/users/me`)
+            .set('Authorization', `Bearer ${token}`)
+            .field("fullname", "John doe")
+            .field("about", "Lorem ipsum")
+            .attach('avatar', mockBuffer, 'video.mp4')
+
+        expect(res.status).toBe(415)
+    })
+
+    it("should successfully update and return the new avatar URL when a valid image file is uploaded", async () => {
+        const mockBuffer = Buffer.from('mock-data');
+
+        const res = await request(app)
+            .patch(`/users/me`)
+            .set('Authorization', `Bearer ${token}`)
+            .field("fullname", "John doe")
+            .field("about", "Lorem ipsum")
+            .attach('avatar', mockBuffer, 'image.png')
+
+        expect(res.status).toBe(200)
+        expect(res.body.user.avatar).toBe("https://cloudinary.com")
+    })
+})
+
+describe("GET /users/me/follow-requests", () => {
+    const [userA, userB, userC] = mockUsers;
+
+    beforeEach(async () => {
+        await prisma.follow.createMany({
+            data: [
+                { fromId: userA.id, toId: userB.id },
+                { fromId: userA.id, toId: userC.id },
+                { fromId: userB.id, toId: userC.id }
+            ]
+        })
+    })
+
+    afterEach(async () => await prisma.follow.deleteMany({}))
+
+    it("should return a list of all incoming follow requests for a user with multiple pending requests", async () => {
+        const token = jwt.sign({ id: userC.id }, env.JWT_SECRET_KEY);
+        const res = await request(app).get("/users/me/follow-requests").set('Authorization', `Bearer ${token}`)
+
+        const followRequests = res.body.followRequests.map((e: Follow) => e.fromId)
+
+        expect(res.status).toBe(200)
+        expect(res.body.followRequests.length).toBe(2)
+        expect(followRequests).toEqual([userA.id, userB.id])
+    })
+
+    it("should return an empty array when the authenticated user has no pending incoming follow requests", async () => {
+        const token = jwt.sign({ id: userA.id }, env.JWT_SECRET_KEY);
+        const res = await request(app)
+            .get("/users/me/follow-requests")
+            .set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(200)
+        expect(res.body.followRequests.length).toBe(0)
+    })
+})
