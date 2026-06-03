9c26907a419034de7c009fc89a6af6ad6d8ccf3c
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-06-03 11:31:48 +0530

    feat: add lucide icon lib and style postcard

--- web/src/components/PostCard.tsx
+++ web/src/components/PostCard.tsx
@@ -1,7 +1,8 @@
 import { Link, useRouter } from "@tanstack/react-router";
-import { useAuth } from "../auth";
+import { useAuth } from "../context/auth";
 import { useState } from "react";
 import type { Post } from "../types";
+import { Heart, MessageCircle } from "lucide-react";
 
 export const PostCard = ({ post }: { post: Post }) => {
   const { user } = useAuth();
@@ -31,29 +32,39 @@ export const PostCard = ({ post }: { post: Post }) => {
   }
 
   return (
-    <div className='border'>
-      <Link to='/users/$userId' params={{ userId: post.userId }}>
-        <img src={post.author.avatar} alt={`${post.author.username}'s avatar`} width={25} />
+    <div className='border-y border-(--app-border) bg-(--app-surface) flex gap-2 p-2'>
+      <Link className="block shrink-0" to='/users/$userId' params={{ userId: post.userId }}>
+        <img className="rounded-full aspect-square" src={post.author.avatar} alt={`${post.author.username}'s avatar`} width={35} />
       </Link>
-      <div>
-        <Link to='/users/$userId' params={{ userId: post.userId }}>
-          <p>{post.author.fullname}</p>
-          <span>{post.author.username}</span>
+      <div className="flex flex-col">
+        <Link className="flex gap-1"
+          to='/users/$userId' params={{ userId: post.userId }}>
+          <p className="font-bold">{post.author.fullname}</p>
+          <p className="text-(--app-muted)/80">@{post.author.username}</p>
         </Link>
         <Link
           to='/posts/$postId'
           params={{ postId: post.id }}
         >{post.text}</Link>
-        <div>
-          <div>
-            <button disabled={isLoading} onClick={likeHandler}>{isLiked ? "Unlike" : "Like"}</button>
+        <div className="pt-1 flex items-center gap-5">
+          <div className="flex items-center">
+            <button
+              disabled={isLoading}
+              onClick={likeHandler}
+              className={`${isLiked ? "text-pink-600" : "inherit"} rounded-full p-1 transition-colors hover:bg-pink-600/20 hover:text-pink-600 disabled:`}>
+              <Heart fill={`${isLiked ? "#db2777" : "var(--app-surface)"}`} size={16} />
+            </button>
             <Link to='/posts/$postId/likes' params={{ postId: post.id }}>{post._count.likes}</Link>
+          </div>
+          <div className="flex items-center ">
             <Link
               to='/posts/$postId'
               params={{ postId: post.id }}
               hash="comment"
-            >Comments</Link>
-            <span> {post._count.comments}</span>
+              className="rounded-full p-1 transition-colors hover:bg-emerald-500/20 hover:text-emerald-500">
+              <MessageCircle size={16} />
+            </Link>
+            <p> {post._count.comments}</p>
           </div>
         </div>
       </div>
