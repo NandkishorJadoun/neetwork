89b8f82d31254fcac5c1693fbd41f5ef0b76cb80
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-06-18 09:12:31 +0530

    test: add configs for test coverage

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         fileParallelism: false,
+        coverage: {
+            provider: "v8",
+            exclude: [
+                "node_modules/**",
+                "coverage/**",
+                "dist/**",
+                "prisma/**",
+                "**/generated/**",
+            ],
+        }
     },
 })
