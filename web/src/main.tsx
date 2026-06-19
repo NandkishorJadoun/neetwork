46bd60b6c837bfafa068b727eaca680223ada034
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-06-19 14:37:15 +0530

    chore: setup tanstack query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,27 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider, useAuth } from './context/auth'
-import { router } from './router'
 import "./styles/index.css"
 import { ThemeProvider } from './context/theme'
+import { createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import { StrictMode } from 'react'
+
+const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  context: {
+    auth: undefined!,
+    queryClient,
+  },
+})
 
 declare module '@tanstack/react-router' {
-  interface Register { router: typeof router }
+  interface Register {
+    router: typeof router
+  }
 }
 
 function InnerApp() {
@@ -15,9 +30,13 @@ function InnerApp() {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <AuthProvider>
-      <InnerApp />
-    </AuthProvider>
-  </ThemeProvider>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AuthProvider>
+          <InnerApp />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
 )
