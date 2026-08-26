1d6a3b770f5d465d7dd7edf4d211827da4bb4e28
Nandkishor Jadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-08-26 19:15:14 +0530

    feat: add better auth generated models in schema

--- apps/api/src/configs/prisma.ts
+++ apps/api/src/configs/prisma.ts
@@ -1,7 +1,6 @@
-import "dotenv/config";
-import { env } from "../schemas/env.schema.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/index.js";
+import { env } from "./env.js";
 
 const connectionString = env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
